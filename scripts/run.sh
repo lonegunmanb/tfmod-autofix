c1481e94a0874df7c4871c191b5348598d3b4e8e
hezijie <lonegunmanb@hotmail.com>
2025-06-18 11:04:41 +0800

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Function to run make target either directly (if in container) or via container
+run_make() {
+  local target="$1"
+
+  if [ -z "$IN_CONTAINER" ]; then
+    $CONTAINER_RUNTIME run --pull always --user "$(id -u):$(id -g)" --rm -v "$(pwd)":/src -w /src -v $AZURE_CONFIG_DIR:/azureconfig -e AZURE_CONFIG_DIR=/azureconfig -e GITHUB_TOKEN -e GITHUB_REPOSITORY -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_CLIENT_ID -e ARM_CLIENT_SECRET mcr.microsoft.com/azterraform make "$target"
+  else
+    make "$target"
+  fi
+}
+
 # if Makefile is not present, download it from the repository
 if [ ! -f Makefile ]; then
     echo "Makefile not found. Downloading from the repository..."
@@ -15,22 +26,10 @@ if [ ! "$(command -v "$CONTAINER_RUNTIME")" ]; then
     exit 1
 fi
 
-if [ -z "$1" ]; then
-    echo "Error: Please provide a make target. See https://github.com/lonegunmanb/tfmod-autofix/blob/main/autofixmakefile for available targets."
-    echo
-    usage
-    exit 1
-fi
-
 # Check if AZURE_CONFIG_DIR is set, if not, set it to ~/.azure
 if [ -z "$AZURE_CONFIG_DIR" ]; then
   AZURE_CONFIG_DIR="$HOME/.azure"
 fi
 
-# Check if we are running in a container
-# If we are then just run make directly
-if [ -z "$IN_CONTAINER" ]; then
-  $CONTAINER_RUNTIME run --pull always --user "$(id -u):$(id -g)" --rm -v "$(pwd)":/src -w /src -v $AZURE_CONFIG_DIR:/azureconfig -e AZURE_CONFIG_DIR=/azureconfig -e GITHUB_TOKEN -e GITHUB_REPOSITORY -e ARM_SUBSCRIPTION_ID -e ARM_TENANT_ID -e ARM_CLIENT_ID -e ARM_CLIENT_SECRET mcr.microsoft.com/azterraform make "$1"
-else
-  make "$1"
-fi
+run_make pre-commit
+run_make pr-check
